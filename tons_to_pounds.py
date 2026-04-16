67c07051f2b879df8562193c437b7150580cd494
Caden Raleigh <Cadenraleigh04@gmail.com>
2026-04-15 21:12:48 -0400

    Add files via upload
    Add Python unit conversion modules and main execution file

---
+++ tons_to_pounds.py
@@ -0,0 +1,4 @@
+import TonstoPoundsFunction as mf 
+tons = float(input("Enter a value in tons to convert to pounds: ")) 
+pounds = convertTonsToPounds(tons) 
+print(tons "tons is =" pounds "pounds")
