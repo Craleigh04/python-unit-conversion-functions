29837c795f8520a2270c0f775db5d1f74cf12c9d
Caden Raleigh <Cadenraleigh04@gmail.com>
2026-04-15 21:14:30 -0400

    Delete tons_to_pounds.py

--- tons_to_pounds.py
+++
@@ -1,4 +0,0 @@
-import TonstoPoundsFunction as mf 
-tons = float(input("Enter a value in tons to convert to pounds: ")) 
-pounds = convertTonsToPounds(tons) 
-print(tons "tons is =" pounds "pounds")
